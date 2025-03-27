034a312b872546aeaa61496882118b4ebc9ffcfd
Vadym Mudryi <vadym@opencrvs.org>
2025-03-27 09:36:58 +0200

    Added cleanup script

--- infrastructure/clear-all-data.k8s.sh
+++ infrastructure/clear-all-data.k8s.sh
@@ -1,3 +1,4 @@
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -14,7 +15,8 @@ print_usage_and_exit () {
     echo 'Usage: ./clear-all-data.sh [-r REPLICAS] [-m MONGODB_HOST] [-e ELASTICSEARCH_HOST] [-n MINIO_HOST]'
     echo ""
     echo "Options:"
-    echo "  -n NAMESPACE               Kubernetes namespace (default: opencrvs-dev)"
+    echo "  -n NAMESPACE               Kubernetes namespace with deployed dependencies (default: opencrvs-deps-dev)"
+    echo "  -o OPENCRVS_NAMESPACE      Kubernetes namespace with deployed OpenCRVS (default: opencrvs-dev)"
     echo "  -r REPLICAS                Number of MongoDB replicas"
     echo "  -m MONGODB_HOST            MongoDB host (default: mongo)"
     echo "  -e ELASTICSEARCH_HOST      Elasticsearch host (default: elasticsearch:9200)"
@@ -29,22 +31,21 @@ print_usage_and_exit () {
     exit 1
 }
 
-if [ -z "$1" ] ; then
-    echo 'Error: Argument REPLICAS is required in position 1.'
-    print_usage_and_exit
-fi
 
 # Default values
 REPLICAS="0"
+NAMESPACE="opencrvs-deps-dev"
+OPENCRVS_NAMESPACE="opencrvs-dev"
 MONGODB_HOST="mongodb"
 ELASTICSEARCH_HOST="elasticsearch"
 MINIO_HOST="minio"
 MINIO_ROOT_USER=minioadmin
 MINIO_ROOT_PASSWORD=minioadmin
 INFLUX_HOST="influxdb"
-while getopts "n:r:m:e:s:i:" opt; do
+while getopts "n:o:r:m:e:s:i:" opt; do
     case $opt in
         n) NAMESPACE=$OPTARG ;;
+        o) OPENCRVS_NAMESPACE=$OPTARG ;;
         r) REPLICAS=$OPTARG ;;
         m) MONGODB_HOST=$OPTARG ;;
         e) ELASTICSEARCH_HOST=$OPTARG ;;
@@ -92,12 +93,15 @@ elasticsearch_host() {
 
 drop_database () {
     local database=${1}
-    kubectl run mongo-drop-job --rm -i --image=mongo:4.4 --restart=Never -- \
+    kubectl run mongo-drop-job --namespace $NAMESPACE --rm -i --image=mongo:4.4 --restart=Never -- \
         mongo $database $(mongo_credentials) --host $HOST --eval "db.dropDatabase()"
 }
 
 # Delete all data from mongo
 #---------------------------
+echo "--------------------------"
+echo "🧹 cleanup mongo databases on $HOST:"
+echo "--------------------------"
 drop_database hearth-dev;
 
 drop_database openhim-dev;
@@ -112,24 +116,38 @@ drop_database performance;
 
 # Delete all data from elasticsearch
 #-----------------------------------
-# TODO: FIX CALL TO INDEX ALIAS
-kubectl run elasticsearch-delete-job --rm -i --image=appropriate/curl --restart=Never -- \
-  curl -XDELETE "http://$(elasticsearch_host)/ocrvs" -v
+
+indices=$(kubectl run elasticsearch-get-index-name --namespace $NAMESPACE --rm -i --image=appropriate/curl --restart=Never --command -- sh -c "curl -sS -XGET http://$(elasticsearch_host)/_cat/indices?h=index" | grep -v pod || true )
+echo "--------------------------"
+echo "🧹 cleanup for indices from $(elasticsearch_host): $indices"
+echo "--------------------------"
+for index in ${indices[@]}; do
+  echo "Delete index $index..."
+  kubectl run elasticsearch-get-index-name-`date +%s` --namespace $NAMESPACE --rm -i --image=appropriate/curl --restart=Never --command -- sh -c "curl -sS -XDELETE http://$(elasticsearch_host)/$index"
+done
+
 
 # Delete all data from metrics
 #-----------------------------
-kubectl run influxdb-drop-job --rm -i --image=appropriate/curl --restart=Never -- \
+echo "--------------------------"
+echo "🧹 cleanup influxdb (metrics) databases on $INFLUX_HOST:"
+echo "--------------------------"
+kubectl run influxdb-drop-job --namespace $NAMESPACE --rm -i --image=appropriate/curl --restart=Never -- \
   curl -X POST http://$INFLUX_HOST:8086/query?db=ocrvs --data-urlencode "q=DROP SERIES FROM /.*/" -v
 
 # Delete all data from minio
 #-----------------------------
-kubectl run minio-delete-job --rm -i --image=minio/mc --restart=Never --command -- sh -c "\
+echo "--------------------------"
+echo "🧹 cleanup MinIO storage on $MINIO_HOST:"
+echo "--------------------------"
+kubectl run minio-delete-job --namespace $NAMESPACE --rm -i --image=minio/mc --restart=Never --command -- sh -c "\
   mc alias set myminio http://$MINIO_HOST:3535 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD && \
   mc rm --recursive --force myminio/ocrvs && \
   mc rb myminio/ocrvs && \
   mc mb myminio/ocrvs"
+echo "All data has been deleted"
 
-# Restart the metabase service
+# Restart the metabase and events services
 #-----------------------------
-# kubectl scale deployment opencrvs-dashboards --replicas=0
-# kubectl scale deployment opencrvs-dashboards --replicas=1
+kubectl delete pod --namespace $OPENCRVS_NAMESPACE -lapp=dashboards
+kubectl delete pod --namespace $OPENCRVS_NAMESPACE -lapp=events
