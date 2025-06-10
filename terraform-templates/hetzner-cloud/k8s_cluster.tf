be7322f14328914752a1698385910defccbdd0aa
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-06-10 20:38:39 +0300

    small fixes

--- terraform-templates/hetzner-cloud/k8s_cluster.tf
+++ terraform-templates/hetzner-cloud/k8s_cluster.tf
@@ -1,10 +1,15 @@
+locals {
+  master_ip = "10.1.1.1"
+}
+
 module "master_node" {
   source             = "./node"
-  ip                 = "10.0.1.1"
-  name               = "${var.country_name}-master-node"
+  ip                 = local.master_ip
+  name               = "${var.country_name}-k8s-master"
   private_network_id = hcloud_network.private_network.id
   location = "hel1"
   user_data = file("cloud-init-master.yaml")
+  depends_on = [ hcloud_network_subnet.private_network_subnet ]
 #   user_data = templatefile("cloud-init-master.yaml", {
 #     users = var.users,
 #     }
@@ -15,13 +20,14 @@ module "worker_node" {
     count = 2
     location = "hel1"
   source             = "./node"
-  name               = "${var.country_name}-worker-node-${count.index}"
+  name               = "${var.country_name}-k8s-worker-${count.index}"
   private_network_id = hcloud_network.private_network.id
   user_data = templatefile(
     "cloud-init-worker.yaml",
     {
       users = var.users,
-      worker_private_ssh_key = file("~/.ssh/opencrvs")
+      master_ip = local.master_ip,
+      worker_private_ssh_key = file("~/.ssh/vmudryi-opencrvs")
     }
   )
   depends_on = [ module.master_node ]
