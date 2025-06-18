6bb0fefb662511bc75f07695fbceda1dc1aef59b
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-06-18 22:59:39 +0300

    fix: cpu type

--- terraform-templates/hetzner-cloud/node/variables.tf
+++ terraform-templates/hetzner-cloud/node/variables.tf
@@ -6,8 +6,7 @@ variable "name" {
 variable "server_type" {
     description = "Type of the server"
     type        = string
-    default     = "cax11"
-  
+    default     = "cx32"
 }
 
 variable "location" {
