434f93649bc048da592d55177362a3b97a720c20
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-07-08 18:23:33 +0300

    fix: Moved cert-manager installation to github runner

--- github-runner/deploy-runner.sh
+++ github-runner/deploy-runner.sh
@@ -1,6 +1,17 @@
 # https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners-with-actions-runner-controller/quickstart-for-actions-runner-controller
 # PUT TOKEN HERE BEFORE RUNNING
 # export GITHUB_PAT=...
+[ "$GITHUB_PAT" == "" ] && \
+   echo "Please add GitHub Personal access token (GITHUB_PAT) before running this script" && exit 1
+echo $GITHUB_PAT
+# Cert manager is a dependency for GitHub Runner
+helm install \
+   cert-manager jetstack/cert-manager \
+   --namespace cert-manager \
+   --create-namespace \
+   --version v1.17.0 \
+   --set crds.enabled=true
+
 helm upgrade --install actions-runner-controller actions-runner-controller/actions-runner-controller \
     --create-namespace \
     --namespace actions-runner-system \
