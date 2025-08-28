01e26bb909288b4454cdb8f7ab68ad929dd1c309
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-08-28 15:03:48 +0300

    feat: Add influxdb restore feature

--- charts/dependencies/files/influxdb/restore.sh
+++ charts/dependencies/files/influxdb/restore.sh
@@ -9,8 +9,8 @@
 #
 # Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
 
-echo "Running restore"
-DATABASES=${DATABASES:-"hearth-dev events user-mgnt application-config metrics webhooks performance"}
+# Database
+DB=ocrvs
 
 # Initial configuration
 RESTORE_DATE=${RESTORE_DATE:-$(date -d "yesterday" +%Y-%m-%d)}
@@ -18,13 +18,11 @@ WORK_PATH=/tmp/backup
 # Remote directory on backup server
 REMOTE_DIR="${BACKUP_REMOTE_DIR:-"/home/$BACKUP_USER"}/$RESTORE_DATE"
 # Temporal archive path inside container
-ARCHIVE_NAME="mongo_backup_${RESTORE_DATE}.tar.gz"
+ARCHIVE_NAME="influxdb_backup_${RESTORE_DATE}.tar.gz"
 ARCHIVE_PATH="/tmp/$ARCHIVE_NAME"
 
 # Install required software to transfer backup on remote host
-rm /etc/apt/sources.list.d/mongodb-org.list
-apt-get update
-apt-get install -y openssh-client rsync
+apt-get update && apt-get install -y openssh-client rsync
 
 transfer_from_backup_host(){
   echo "[$(date +%F\ %H:%M:%S)] Transfer backup from remote host"
@@ -44,6 +42,7 @@ decrypt_backup() {
   openssl enc -d -aes-256-cbc -pbkdf2 -salt -in "${ARCHIVE_PATH}.enc" -out "$ARCHIVE_PATH" -pass env:ENCRYPT_PASS
   echo "[$(date +%F\ %H:%M:%S)] Decrypted archive at $ARCHIVE_PATH"
 }
+
 extract_backup(){
   mkdir -p "$WORK_PATH"
   tar -zxvf "$ARCHIVE_PATH" -C "$WORK_PATH"
@@ -51,37 +50,12 @@ extract_backup(){
   ls -hl $WORK_PATH
 }
 
-restore_databases() {
-  echo "[$(date +%F\ %H:%M:%S)] Running restore databases"
-  for DB in $DATABASES; do
-    DB_ARCHIVE_PATH="$WORK_PATH/$DB.gz"
-    if [ -f "$DB_ARCHIVE_PATH" ]; then
-      echo "[$(date +%F\ %H:%M:%S)] Restoring database: $DB"
-      if [ -n "${MONGODB_ADMIN_USER:-}" ] && [ -n "${MONGODB_ADMIN_PASSWORD:-}" ]; then
-        mongorestore --host "$MONGODB_HOST" \
-          --username "$MONGODB_ADMIN_USER" \
-          --password "$MONGODB_ADMIN_PASSWORD" \
-          --authenticationDatabase admin \
-          --gzip --archive="$DB_ARCHIVE_PATH" --nsInclude "$DB.*" --drop
-      else
-        mongorestore --host "$MONGODB_HOST" \
-          --gzip --archive="$DB_ARCHIVE_PATH" --nsInclude "$DB.*" --drop
-      fi
-    else
-      echo "[$(date +%F\ %H:%M:%S)] [WARN] Archive for $DB not found: $DB_ARCHIVE_PATH" >&2
-    fi
-  done
+restore(){
+  echo "[$(date +%F\ %H:%M:%S)] Running restore for DB $DB"
+  influxd restore -portable -host $INFLUXDB_HOST:$INFLUXDB_HOST -db $DB $WORK_PATH
 }
-
-
-echo "[$(date +%F\ %H:%M:%S)] Running restore"
-
-transfer_from_backup_host
-
-decrypt_backup
-
-extract_backup
-
 /scripts/cleanup.sh
-
-restore_databases
+transfer_from_backup_host
+decrypt_backup
+extract_backup
+restore
