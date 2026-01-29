19cf61beb13941be2e4e60340983e660f2a0da77
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-01-29 15:11:49 +0200

    feat: Provide way to manage SSL certificate from configuration script

--- infrastructure/environments/utils.ts
+++ infrastructure/environments/utils.ts
@@ -1,3 +1,7 @@
+import {
+  Secret,
+  Variable
+} from './github'
 
 export function generateLongPassword() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -5,4 +9,18 @@ export function generateLongPassword() {
   for (let i = 16; i > 0; --i)
     result += chars[Math.floor(Math.random() * chars.length)]
   return result
+}
+
+export function findExistingValue<T extends string>(
+  name: string,
+  type: T,
+  scope: 'ENVIRONMENT' | 'REPOSITORY',
+  existingValues: Array<Secret | Variable>
+) {
+  return existingValues.find(
+    (value) =>
+      value.name === name && value.type === type && value.scope === scope
+  ) as
+    | (T extends 'SECRET' ? Secret : T extends 'VARIABLE' ? Variable : never)
+    | undefined
 }
