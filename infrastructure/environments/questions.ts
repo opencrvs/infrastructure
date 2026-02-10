c08df9f4c1bbeab6d0eb99a8300c15fbeda343ef
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-02-10 10:52:53 +0200

    fix: Store backup host as variable

--- infrastructure/environments/questions.ts
+++ infrastructure/environments/questions.ts
@@ -210,7 +210,7 @@ export const backupQuestions = [
     type: 'text' as const,
     message:
       `Please enter backup server host/IP address, (default: no backup):`,
-    valueType: 'SECRET' as const,
+    valueType: 'VARIABLE' as const,
     // validate: notEmpty,
     valueLabel: 'BACKUP_HOST',
     initial: process.env.BACKUP_HOST || '',
