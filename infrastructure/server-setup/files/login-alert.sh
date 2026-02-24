17cf462e6ee67eae35c35fbc862ebec3c1fdcab1
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-02-24 18:15:45 +0200

    fix: Improve backup emails

--- infrastructure/server-setup/files/login-alert.sh
+++ infrastructure/server-setup/files/login-alert.sh
@@ -20,28 +20,42 @@ exec 2> >(logger -t login-alert -p user.err)
 START_TIME=$(cat "$TIMESTAMP_FILE")
 
 NEW_LOGINS=$(ausearch -if /var/log/audit/audit.log --start $START_TIME -m USER_LOGIN | grep 'res=success')
+
 if [ ! -z "$NEW_LOGINS" ]; then
     echo "Sending email to $EMAIL_TO"
 
-    # Create the email file
+    BODY="<html>
+    <body>
+    <p>New login detected on host $HOSTNAME:</p>
+    <pre>$NEW_LOGINS</pre>
+    </body>
+    </html>"
+
     EMAIL_FILE=$(mktemp)
-    cat > "$EMAIL_FILE" <<EOF
+    cat >"$EMAIL_FILE" <<EOF
 From: $EMAIL_FROM
 To: $EMAIL_TO
 Subject: [Alert] New SSH Login on $HOSTNAME
+MIME-Version: 1.0
+Content-Type: text/html; charset=UTF-8
 
-$NEW_LOGINS
+$BODY
 EOF
+    # Build the curl command based on SMTP_SECURE
+    CURL_OPTS=(
+        --url "smtp://$SMTP_SERVER:$SMTP_PORT"
+        --mail-from "$EMAIL_FROM"
+        --mail-rcpt "$EMAIL_TO"
+        --upload-file "$EMAIL_FILE"
+        --user "$SMTP_USER:$SMTP_PASS"
+        --silent --show-error
+    )
 
-    # Send email via curl using SMTP
-    curl --url "smtp://$SMTP_SERVER:$SMTP_PORT" \
-         --ssl \
-         --mail-from "$EMAIL_FROM" \
-         --mail-rcpt "$EMAIL_TO" \
-         --upload-file "$EMAIL_FILE" \
-         --user "$SMTP_USER:$SMTP_PASS" \
-         --silent --show-error
+    if [[ "$SMTP_SECURE" == "true" ]]; then
+        CURL_OPTS+=(--ssl)
+    fi
 
-    rm "$EMAIL_FILE"   # Clean up temp file
+    curl "${CURL_OPTS[@]}"
+    rm "$EMAIL_FILE"
 fi
 date +%T > $TIMESTAMP_FILE
